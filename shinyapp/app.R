4550351164ee8826b5661c7a4f5438324567ac29
Nick Sapoval <nikolshapoval@gmail.com>
2021-10-13 08:03:34 -0500

    Finished draft Shiny app

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -6,7 +6,7 @@ host = "mongodb://sno.cs.rice.edu:27017"
 databaseName <- "cov2db"
 collectionName <- "annotated_vcf"
 
-loadData <- function(gene) {
+loadData <- function(gene, min_af, max_af, pos, mtype, pchange, impact) {
     # Connect to the database
     db <- mongo(collection = collectionName,
                 url = sprintf(
@@ -16,15 +16,28 @@ loadData <- function(gene) {
                 ),
                 options = ssl_options(weak_cert_validation = TRUE))
     
-    if (gene == "all") {
-        data <- db$find(query='{"chromosome": "NC_045512.2"}')
-    } else {
-        data <- db$find(query=sprintf('{"chromosome": "NC_045512.2", "info_GeneName": "%s"}', gene))
+    query <- '{"chromosome": "NC_045512.2"'
+    if (gene != "all") {
+        query <- sprintf('%s, "info_GeneName": "%s"', query, gene)
     }
+    if (pos != -1) {
+        query <- sprintf('%s, "start": %s', query, pos)
+    }
+    if (mtype != "all") {
+        query <- sprintf('%s, "info_SequenceOntology": "%s"', query, mtype)
+    }
+    if (pchange != "all") {
+        query <- sprintf('%s, "info_ProteinChange": "%s"', query, pchange)
+    }
+    if (impact != "all") {
+        query <- sprintf('%s, "info_PutativeImpact": "%s"', query, impact)
+    }
+    query <- sprintf('%s, "info_af": {"$gte": %s}, "info_af": {"$lte": %s}', query, min_af, max_af)
+    query <- sprintf('%s}', query)
+    data <- db$find(query=query)
     return(data);
 }
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
     fluidRow(
         column(3, 
@@ -36,7 +49,7 @@ ui <- fluidPage(
         column(3,
                textInput(inputId = "gene_name",
                        label = "Gene of interest (or all)",
-                       value = "all")),
+                       value = "S")),
         column(3,
                textInput(inputId = "mutation_type",
                          label = "Mutation type (or all)",
@@ -53,55 +66,52 @@ ui <- fluidPage(
         column(3,
                numericInput(inputId = "allele_frequency_min",
                             label = "Minimum allele frequncy",
-                            value = 0.0,
+                            value = 0.05,
                             min = 0.0,
                             max = 1.0,
                             step = 0.01)),
         column(3,
                numericInput(inputId = "allele_frequency_max",
                             label = "Maximum allele frequncy",
-                            value = 0.0,
+                            value = 1.0,
                             min = 0.0,
                             max = 1.0,
                             step = 0.01)),
         column(3,
-               textInput(inputId = "allele_frequency_min",
-                            label = "Reference amino acid",
+               textInput(inputId = "protein_change",
+                            label = "Amino acid change",
                             value = "all")),
         column(3,
-               textInput(inputId = "allele_frequency_max",
-                            label = "Alterantive amino acid",
+               textInput(inputId = "putative_impact",
+                            label = "Putative impact",
                             value = "all"))
     ),
-               
+
+    hr(),
     
-    
-    #fluidRow(
-    #    column(12,
-    #           dataTableOutput('table')
-    #    )
-    #)
+    plotOutput('hist_type'),
     
     hr(),
     
-    #plotOutput('hist_af'),
-    plotOutput('hist_type')
+    dataTableOutput('table')
     
 )
 
 server <- function(input, output) {
-    data <- reactive({loadData(gene=input$gene_name)})
+    data <- reactive({d <- loadData(gene=input$gene_name, 
+                                    min_af=input$allele_frequency_min,
+                                    max_af=input$allele_frequency_max,
+                                    pos=input$position_nt,
+                                    mtype=input$mutation_type,
+                                    pchange=input$protein_change,
+                                    impact=input$putative_impact)
+                      d$info_ann <- NULL
+                      d$chromosome <- NULL
+                      d$id <- NULL
+                      d$filter <- NULL
+                      return(d)})
     
     output$table <- renderDataTable(data())
-    
-    #output$hist_af <- renderPlot({
-    #    req(data)
-    #    ggplot(data(), aes(x=info_af, stat=count)) +
-    #        geom_histogram()
-             #main="Distribution of allele frequencies",
-             #xlab="Allele frequency",
-             #ylab="Number of variants")
-    #}) 
     
     mut_type_table <- reactive(table(data()["info_SequenceOntology"]))
     
@@ -109,7 +119,12 @@ server <- function(input, output) {
         req(data)
         barplot(mut_type_table(),
                 main="Distribution of mutation types",
-                ylab="Number of variants")
+                ylab="Number of variants",
+                col=rgb(0.2,0.4,0.6,0.8),
+                cex.axis=2,
+                cex.lab=1.5,
+                cex.names=1.2,
+                cex.main=2)
     })
 }
 

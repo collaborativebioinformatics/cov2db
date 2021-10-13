1f8fb440bb0beeee7377f5211472bef102173f8b
RPS <rps@RPSs-MacBook-Air.local>
2021-10-13 09:25:13 -0400

    Ramanandan added Bash script for running WDL

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,2 +1,2 @@
 # We need to get the cromwell server path
-java -Dconfig.file=cromwell.conf -jar $CROMWELL run -i input.json ../wdl/SRA_FASTQ_Files_Fetcher.wdl
+java -Dconfig.file=cromwell.conf -jar $CROMWELL run -i input.json ../wdl/Cov2DB_FASTQ2JSON_Pipeline.wdl
